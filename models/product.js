6893ffd210c54b5fefc0ebe2b6188fadeb2ad2a2
shanibamariyam <shanibamariyam00@gmail.com>
2024-06-10 22:31:54 +0530

    completed

---
+++ models/product.js
@@ -0,0 +1,11 @@
+const mongoose=require("mongoose")
+const schema=mongoose.Schema(
+    {
+        "productname":String,
+        "productid":String,
+        "price":String,
+        "description":String
+    }
+)
+let productmodel=mongoose.model("products",schema)
+module.exports={productmodel}
